8bd669b1bbf1dda9688a2d7f06683164402e2372
ashishivanweb-art <ashish.ivanweb@gmail.com>
2026-03-02 21:42:43 +0530

    backend update

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -7,12 +7,18 @@ import {
   startInnings,
   getMySeries,
   getMatchesOfSeries,
-  getMyTeams
+  getMyTeams,
+  updateTeam,
+  deleteTeam,
+  updateSeries,
+  deleteSeries,
+  updateMatch,
+  deleteMatch,
 } from '../controllers/admin.controller.js';
 
 import adminAuth from '../middleware/adminAuth.js';
 
-import { addPlayer, getPlayersByTeam, updatePlayer } from '../controllers/player.controller.js';
+import { addPlayer, getPlayersByTeam, removePlayer, updatePlayer } from '../controllers/player.controller.js';
 import { addBall, editBall, undoLastBall } from '../controllers/ball.controller.js';
 import Match from '../models/match.js';
 
@@ -27,25 +33,38 @@ router.put('/match/pause/:matchId', async (req, res) => {
   res.json({ message: 'Match paused' });
 });
 
+// Teams
 router.post('/add-team',adminAuth, createTeam);
 router.get('/get-teams', adminAuth, getMyTeams);
+router.put('/update-team/:id', adminAuth, updateTeam);
+router.delete('/delete-team/:id', adminAuth, deleteTeam)
 
+// Match
 router.post('/add-match',adminAuth, createMatch);
 router.get('/series/:seriesId/matches',adminAuth, getMatchesOfSeries);
+router.put('/update-match/:id', adminAuth, updateMatch);
+router.delete('/delete-match/:id', adminAuth, deleteMatch);
 
-
-router.post('/series', adminAuth, addSeries);
+// series
+router.post('/add-series', adminAuth, addSeries);
 router.get('/get-my-series', adminAuth, getMySeries);
+router.put('/update-series/:id', adminAuth, updateSeries)
+router.delete('/delete-series/:id', adminAuth, deleteSeries);
+
+
+//Toss and Innings
+router.put('/match/toss/:matchId', adminAuth, updateToss);
+router.put('/match/start-innings/:matchId',adminAuth, startInnings);
 
 
 //Player Routes
 router.post('/add-player', adminAuth, addPlayer);
 router.get('/get-players', adminAuth, getPlayersByTeam);
 router.put('/player/:id', adminAuth, updatePlayer);
+router.delete('/team/:teamId/player/:playerId', adminAuth, removePlayer);
 
-router.put('/match/toss/:matchId', adminAuth, updateToss);
-router.put('/match/start-innings/:matchId',adminAuth, startInnings);
 
+// Balls
 
 router.post('/matches/:matchId/balls',adminAuth, addBall);
 router.delete("/matches/:matchId/last-ball",adminAuth, undoLastBall);
